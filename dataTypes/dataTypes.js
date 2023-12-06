2f3a7256f90c1abfd3c600b03f0f5f7dc714044d
vimal joshi <111500483+vimaljoshi172003@users.noreply.github.com>
2023-12-06 17:21:40 +0000

    DataTypes in js

---
+++ dataTypes/dataTypes.js
@@ -0,0 +1,23 @@
+"use strict";//treat all JS coe as newer version;
+
+// alert(3+3) // we are Useing nodejs,not browser
+
+
+// DataTypes in js
+let Number = 4;
+let string = "Hello How are you";
+let isLoggedIn = false;
+let Name;
+
+//number => 2 to powe 53
+//bigint 
+//string=>""f
+//boolean => true/false
+//null
+//undefined
+//symbol =>unique
+// Object
+
+console.log(typeof isLoggedIn)
+console.log(typeof null)
+console.log(typeof undefined)
