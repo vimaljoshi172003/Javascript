c2522ce651476b80b716331c80107611eb3b616a
vimal joshi <111500483+vimaljoshi172003@users.noreply.github.com>
2023-12-07 13:00:37 +0000

    Conversion

---
+++ conversion/conversion.js
@@ -0,0 +1,28 @@
+let score = "69abc";
+console.log(typeof score);
+
+
+// convert my String to number
+let MyNumber = Number(score);
+console.log(typeof MyNumber);
+console.log(MyNumber);
+console.log(typeof NaN);
+
+// "33"=>33
+// "33abc"=>NaN
+// true => 1; false => 0;
+
+let isLoggedIn = 0;
+let Myboolean =Boolean(isLoggedIn);
+console.log(Myboolean);
+
+// 1 => true; 0 => false
+// "" => false; "vimal" => true
+
+let numberIs = 69;
+let Mystring = String(numberIs);
+console.log(typeof Mystring);
+
+
+
+
