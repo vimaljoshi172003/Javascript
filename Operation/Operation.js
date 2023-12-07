ed4b69c62a1aee6dc6acb6b5624af62a1ec8fa23
vimal joshi <111500483+vimaljoshi172003@users.noreply.github.com>
2023-12-07 13:43:26 +0000

    Operation In Js

---
+++ Operation/Operation.js
@@ -0,0 +1,21 @@
+let Myvalue = 69;
+let NegValue = -Myvalue;
+
+// console.log(NegValue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " Vimal"
+console.log(str1+str2)
+console.log("1" +2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+console.log(+true);
+console.log(+"");
+
