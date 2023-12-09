0defa248d34cb95af59b718d967dc4d81e5a7521
vimal joshi <111500483+vimaljoshi172003@users.noreply.github.com>
2023-12-09 14:09:58 +0000

    Number&Math in js

---
+++ dataTypes/Number_Math.js
@@ -0,0 +1,30 @@
+const score =400;
+// console.log(score);
+
+const balance = new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const oterNumber = 2312.897;
+// console.log(oterNumber.toPrecision(4));
+
+const hundreds = 100000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// ########################## MATHS #################################
+const arry = [4,3,6,1,3]
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.floor(4.9));
+// console.log(Math.ceil(4.1));
+// console.log(Math.min(4,3,6,1,3))
+// console.log(Math.max(4,5,2,7,8))
+// console.log(Math.round(Math.random()*10)+1)
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random()*(max-min+1)+min))
+console.log((max-min+1)+min)
