01f6732afbc8ad3ad2bbb3f9ecdb740b91618e93
vimal joshi <111500483+vimaljoshi172003@users.noreply.github.com>
2023-12-10 15:35:54 +0000

    array And its Methods

---
+++ arrayInJs/array.js
@@ -0,0 +1,27 @@
+const myArr = [0,4,2,1,5,7,8];
+const heroes = ["batMan","superMan"];
+const myArr2 = new Array(2,4,7,8);
+// console.log(myArr);
+// console.log(heroes);
+// console.log(myArr);
+
+// Array Methods;
+// myArr.push(6);
+// myArr.push(19);
+// myArr.pop();
+// myArr.unshift(9);
+// console.log(myArr.includes(0));
+const newArray =myArr.join();
+// console.log(newArray);
+// console.log(myArr);
+const myNewArray = [0,1,2,3,4,5,6]
+console.log("A",myNewArray);
+console.log(myNewArray.slice(0,3));
+console.log(myNewArray);
+const myNewArray2 = [0,1,2,3,4,5,6];
+console.log("B",myNewArray2);
+console.log(myNewArray2.splice(0,3));
+console.log(myNewArray2);
+
+//  in slice no Change in original array
+//  in splice manipulation in original
