43248d9e914a1be8475622b2102087b5291287f7
vimal joshi <111500483+vimaljoshi172003@users.noreply.github.com>
2023-12-12 13:59:02 +0000

    Json

---
+++ Object/JsonInJs.js
@@ -0,0 +1,18 @@
+// de-Structuring object in javascript
+const myObj = {
+    Name:"vimal",
+    course:"BCA",
+    
+}
+const {Name:YourName} = myObj;
+console.log(YourName);
+//Json Object
+// {
+//     "batter":
+//         [
+//             { "id": "1001", "type": "Regular" },
+//             { "id": "1002", "type": "Chocolate" },
+//             { "id": "1003", "type": "Blueberry" },
+//             { "id": "1004", "type": "Devil's Food" }
+//         ]
+// }
