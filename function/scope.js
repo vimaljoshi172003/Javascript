2940cbaf917cc0019c650a74e149a3525b3fe0f2
vimal joshi <111500483+vimaljoshi172003@users.noreply.github.com>
2023-12-13 14:01:45 +0000

    function and scope in js

---
+++ function/scope.js
@@ -0,0 +1,29 @@
+var c =39
+if (true) {
+    let a = 10;
+    const b = 20;
+    var c =30;
+}
+// console.log(a);
+// console.log(b);
+console.log(c);
+
+function One(){
+    const Username = "Vimal Joshi";
+    function Two(){
+        const website = "Instagrame";
+        console.log(Username);
+    }
+    // console.log(website);
+    Two();
+}
+One();
+if(true){
+    const Username = "JoshiJi "
+    if(Username === "JoshiJi "){
+        const MyName = "Vimal Joshi"
+        console.log(Username+MyName)
+    }
+    // console.log(MyName);
+}
+// console.log(Username);
