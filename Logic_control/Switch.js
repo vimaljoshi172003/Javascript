97aee1e2abde0ac760cdef69afcb4ac78a5d1025
vimal joshi <111500483+vimaljoshi172003@users.noreply.github.com>
2023-12-17 13:05:17 +0000

    switch in js

---
+++ Logic_control/Switch.js
@@ -0,0 +1,21 @@
+let month = "March"
+switch (month) {
+    case "january":
+        console.log("january");
+        break;
+    case "Feb":
+        console.log("Feb");
+        break;
+    case "March":
+        console.log("March");
+        break;
+    case "april":
+        console.log("april");
+        break;
+    case "may":
+        console.log("may");
+        break;
+    default:
+        console.log("defult case ");
+        break;
+}
