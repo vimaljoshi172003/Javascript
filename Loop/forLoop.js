550a56452bb73a5d7b8d301bf7dcfd1aa2dd83d4
vimal joshi <111500483+vimaljoshi172003@users.noreply.github.com>
2023-12-18 10:07:36 +0000

    loops in js

---
+++ Loop/forLoop.js
@@ -0,0 +1,20 @@
+for (let i = 0; i < 10; i++) {
+    const element = i;
+    if (element == 5) {
+        // console.log("5 is the best number");
+    }
+    // console.log(element);
+}
+for (let i = 1; i <= 10; i++) {
+    // console.log(`table of ${i} is`);
+    for (let j = 1; j <= 10; j++) {
+        // console.log(`${i} X ${j} = ${i*j}`);
+        
+    }
+}
+let myArray= ["Batman","superMan","Hulk"];
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    // console.log(element);
+    
+}
