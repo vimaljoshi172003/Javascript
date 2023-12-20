0ad95769d53c586d07295cf995c8f2f13bb26b82
vimal joshi <111500483+vimaljoshi172003@users.noreply.github.com>
2023-12-20 06:56:29 +0000

    Loops

---
+++ Loop/While&dowhile.js
@@ -0,0 +1,16 @@
+let index = 0
+// while(index <= 10){
+//     console.log(`the value of index is ${index}`);
+//     index = index + 2 ;
+// }
+let Array1 = ["Batman","Superman","Flash"]
+let Arr = 0
+while (Arr<Array1.length) {
+    // console.log(`the value is ${Array1[Arr]}`);
+    Arr = Arr +1;
+}
+let num =11;
+do{
+    // console.log(`my number is ${num}`);
+    num++
+}while (num <= 10) 
