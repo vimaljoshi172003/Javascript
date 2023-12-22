0922fee86bc57c17d8b19c8a55eb0c8832f3f026
vimal joshi <111500483+vimaljoshi172003@users.noreply.github.com>
2023-12-22 12:41:50 +0530

    Add files via upload
    ForIn loop in js

---
+++ Loop/ForIn.js
@@ -0,0 +1,11 @@
+const Myobj = {
+    Name:"Vimal",
+    Course:"Bca"
+}
+for (const key in Myobj) {
+    // console.log(`the ${key} Of  value is ${Myobj[key]}`);
+}
+const Myarray = ["js","python","c++"]
+for (const Arry in Myarray) {
+    console.log(`The index ${Arry} of value is ${Myarray[Arry]}`);
+}
