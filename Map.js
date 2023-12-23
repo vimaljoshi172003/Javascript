438bbf65f20e686b3580fac5bdb0222b68f00c6e
vimal joshi <111500483+vimaljoshi172003@users.noreply.github.com>
2023-12-23 20:28:49 +0530

    Maps in js

---
+++ Map.js
@@ -0,0 +1,9 @@
+const mynums = [1,2,3,4,5,6,7,8,9,10];
+// const newNums = mynums.map((nums)=>{
+//     return nums+10
+// });
+const newNums = mynums
+                .map((nums)=>nums*10)
+                .map((nums)=>nums+1)
+                .filter((nums)=>nums>=40)
+console.log(newNums);
